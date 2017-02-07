24d74a2fb50ae9592c6c0bfc387604a4d3ba1e66
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-07 12:33:46 +0000

    refine func

--- yxl-projectile.el
+++ yxl-projectile.el
@@ -4,9 +4,14 @@
 
 
 (defun yxl-projectile-find-file ()
-  "Only find files in git projects."
+  "Special case of finding file in projects:
+Only find files in a git project. Useful when operate in nested projects:
+use the normal version to find file in a projectile project, and use this version
+to find files in the super project."
   (interactive)
-  (let* ((projectile-project-root-files-bottom-up '(".git")))
+  (let* ((projectile-project-root-files-bottom-up '(".git"))
+         (projectile-project-root (projectile-root-bottom-up default-directory))
+         (projectile-project-name projectile-project-root))
     (counsel-projectile-find-file)))
 
 
