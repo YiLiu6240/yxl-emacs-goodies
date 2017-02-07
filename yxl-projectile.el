fc56c7a5bed0f7c0c87b9d893249453d3eb6ce08
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-07 19:04:34 +0000

    rm package
    The caching in projectile makes hijacking it with "let" not working properly.
    Better use ffip as a second project search package.

--- yxl-projectile.el
+++
@@ -1,19 +0,0 @@
-(require 'projectile)
-(require 'counsel-projectile)
-
-
-
-(defun yxl-projectile-find-file ()
-  "Special case of finding file in projects:
-Only find files in a git project. Useful when operate in nested projects:
-use the normal version to find file in a projectile project, and use this version
-to find files in the super project."
-  (interactive)
-  (let* ((projectile-project-root-files-bottom-up '(".git"))
-         (projectile-project-root (projectile-root-bottom-up default-directory))
-         (projectile-project-name projectile-project-root))
-    (counsel-projectile-find-file)))
-
-
-
-(provide 'yxl-projectile)
