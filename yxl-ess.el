36b4c543dc856a8e1de277fdd5430b53aba31a38
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-26 17:53:51 +0000

    improve yxl-ess

--- yxl-ess.el
+++ yxl-ess.el
@@ -1,4 +1,5 @@
 (require 'ess-site)
+(require 'ivy)
 
 (defun yxl-ess-rdired-str ()
   (interactive)
@@ -20,6 +21,7 @@
   (yxl-ess-execute-func-at-point r-func))
 
 ;; my own custom funcs in R
+;; TODO: these functions should be defined from emacs?
 
 (defun yxl-ess-exec-lsos ()
   "invoke lsos function, this function needs to be in environment"
@@ -30,5 +32,18 @@
   "invoke lsos function, this function needs to be in environment"
   (interactive)
   (ess-execute "lsdf()"))
+
+(setq yxl-ess-useful-funcs
+      '(("sessionInfo() -- info on loaded pacakges" . "sessionInfo()")
+        ("lsdf() -- list current dataframes" . "lsdf()")
+        ("lsos()" . "lsos")))
+
+(defun yxl-ess-call-useful-funcs ()
+  (interactive)
+  (ivy-read "Call useful funcs:"
+            yxl-ess-useful-funcs
+            :action (lambda (x) (ess-execute (cdr x)))
+            :caller 'yxl-ess-call-useful-funcs))
+
 
 (provide 'yxl-ess)
