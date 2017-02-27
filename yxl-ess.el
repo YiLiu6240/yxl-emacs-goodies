9bfb30f125ceca38f07a2e2205251421902cfdad
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-27 13:24:24 +0000

    overhual yxl-ess
    now contains two major functions (and their respective function list):
    - yxl-ess-call-usefull-funcs
    - yxl-ess-call-atpoint-usefull-funcs

--- yxl-ess.el
+++ yxl-ess.el
@@ -1,29 +1,35 @@
 (require 'ess-site)
 (require 'ivy)
 
-(defun yxl-ess-rdired-str ()
-  (interactive)
-  (let ((objname (ess-rdired-object)))
-    (ess-execute (concat "str(" objname ")\n"))))
+;; TODO:
+;; add optional arguments
+(defvar yxl-ess-useful-funcs
+  '(("sessionInfo() -- info on loaded pacakges" . "sessionInfo()")
+    ("lsdf() -- list current dataframes -- user-defined" . "lsdf()")
+    ("lsos() -- list object by size -- user-defined" . "lsos()"))
+  "list of useful R functions to execute")
 
-(defun yxl-ess-execute-func-at-point (r-func)
+(defvar yxl-ess-useful-atpoint-funcs
+  '(("str()" . "str")
+    ("dplyr::summarize()" . "dplyr::summarize")
+    ("summary()" . "summary")
+    ("dim()" . "dim")
+    ("dimnames()" . "dimnames"))
+  "list of useful R functions to execute to the current object atpoint.")
+
+(defun yxl-ess-call-atpoint-func (r-func)
   (let ((objname (current-word)))
     (if objname
         (progn
           (ess-execute (concat r-func "(" objname ")"))))))
 
-(defun yxl-ess-at-point-str ()
+(defun yxl-ess-call-atpoint-str ()
   (interactive)
-  (yxl-ess-execute-func-at-point "str"))
+  (yxl-ess-call-atpoint-func "str"))
 
-(defun yxl-ess-at-point-generic (r-func)
+(defun yxl-ess-call-atpoint-generic (r-func)
   (interactive "sR function to execute: ")
-  (yxl-ess-execute-func-at-point r-func))
-
-(setq yxl-ess-useful-funcs
-      '(("sessionInfo() -- info on loaded pacakges" . "sessionInfo()")
-        ("lsdf() -- list current dataframes -- user-defined" . "lsdf()")
-        ("lsos() -- list object by size -- user-defined" . "lsos()")))
+  (yxl-ess-call-atpoint-func r-func))
 
 (defun yxl-ess-call-useful-funcs ()
   (interactive)
@@ -31,5 +37,17 @@
             yxl-ess-useful-funcs
             :action (lambda (x) (ess-execute (cdr x)))
             :caller 'yxl-ess-call-useful-funcs))
+
+(defun yxl-ess-call-atpoint-useful-funcs ()
+  (interactive)
+  (ivy-read "Call useful funcs:"
+            yxl-ess-useful-atpoint-funcs
+            :action (lambda (x) (yxl-ess-call-atpoint-func (cdr x)))
+            :caller 'yxl-ess-call-atpoint-useful-funcs))
+
+(defun yxl-ess-rdired-str ()
+  (interactive)
+  (let ((objname (ess-rdired-object)))
+    (ess-execute (concat "str(" objname ")\n"))))
 
 (provide 'yxl-ess)
