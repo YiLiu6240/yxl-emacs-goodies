6c2b1d78f8c8f47ab78c7270367e12087ab611dd
YiLiu6240 <yi.liu6240@gmail.com>
2017-03-01 18:48:10 +0000

    format

--- yxl-elfeed.el
+++ yxl-elfeed.el
@@ -142,14 +142,20 @@ If no prefix arg: select tags from database; otherwise asks for input"
                "Fallback"
              :match (lambda (_candidate) t)
              :candidates '(("Default filter" .
-                            (lambda (x) (elfeed--read-tag
-                                         (default-value 'elfeed-search-filter))))
+                            (lambda (x)
+                              (elfeed--read-tag (default-value
+                                                  'elfeed-search-filter))))
                            ("Default filter: append" .
-                            (lambda (x) (elfeed--read-tag
-                                         (concat (default-value 'elfeed-search-filter) " " x))))
+                            (lambda (x)
+                              (elfeed--read-tag
+                               (concat (default-value
+                                         'elfeed-search-filter) " " x))))
                            ("Current filter: append" .
-                            (lambda (x) (elfeed--read-tag (concat elfeed-search-filter " " x))))
-                           ("Manual filter" . (lambda (x) (elfeed--read-tag x))))
+                            (lambda (x)
+                              (elfeed--read-tag
+                               (concat elfeed-search-filter " " x))))
+                           ("Manual filter" .
+                            (lambda (x) (elfeed--read-tag x))))
              :action (lambda (x) (funcall x helm-pattern))))
         :buffer "*Helm Elfeed Search*"))
 
