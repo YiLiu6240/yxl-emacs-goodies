7f64bc35348121291425d9d4bc5d8e308c361d71
YiLiu6240 <yi.liu6240@gmail.com>
2017-05-17 09:24:20 +0100

    use alist for yxl-ia-list
    - can specify an app name different from the underlying function
    - sort app name

--- yxl-invoke-applications.el
+++ yxl-invoke-applications.el
@@ -1,16 +1,20 @@
 (require 'ivy)
 
 (defvar yxl-ia-list
-  '("calendar" "org-agenda" "org-capture")
+  '(("calendar" . calendar)
+    ("org-agenda" . org-agenda)
+    ("org-capture" . org-capture))
   "List of registered 'applications' to be invoked by
-`yxl-invoke-applications'. Elements should be the string of the command,
-to be able to sort.")
+`yxl-invoke-applications'. Elements should be as '('app-name' . func-name).")
 
 (defun yxl-invoke-applications ()
   (interactive)
   (ivy-read "Invoke applications:"
-            (sort (delete-dups yxl-ia-list) #'string<)
-            :action (lambda (x) (funcall (intern x)))
+            (sort (delete-dups yxl-ia-list) (lambda (elem1 elem2)
+                                              (let ((str1 (car elem1))
+                                                    (str2 (car elem2)))
+                                                (string-lessp str1 str2))))
+            :action (lambda (x) (funcall (cdr x)))
             :caller 'yxl-invoke-select))
 
 (provide 'yxl-invoke-applications)
